16f4aac8e1eccd21c6fbb02d2d0d2c3cfe58ae97
Amancio <amancio.jluis@outlook.com>
2025-10-23 14:05:27 -0300

    JLA - TestBeyond: testes de login, camadas: support ; services

---
+++ playwright.config.js
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+export default defineConfig({
+  testDir: './playwright/e2e',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    // baseURL: 'http://localhost:3000',
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'api-tests',
+    },
+  ],
+
+});
+
---
+++ playwright/e2e/auth/register.spec.js
@@ -0,0 +1,101 @@
+import { test, expect } from 'playwright/test';
+import { getUser } from '../../support/factories/user.js';
+import { registerService } from '../../support/services/register.js';
+
+
+test.describe('POST / auth/register', () => {
+
+    let register = null;
+
+    test.beforeEach(({ request }) => {
+        register = registerService(request);
+    })
+
+    test('Deve cadastrar um novo usuário', async ({ request }) => {
+
+        const user = getUser();
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(201);
+
+        const responseBody = await response.json();
+        expect(responseBody.message).toBe('Usuário cadastrado com sucesso!');
+        expect(responseBody).toHaveProperty('message', 'Usuário cadastrado com sucesso!');
+        expect(responseBody.user).toHaveProperty('id');
+        expect(responseBody.user).toHaveProperty('name', user.name);
+        expect(responseBody.user).toHaveProperty('email', user.email);
+        expect(responseBody.user).not.toHaveProperty('password');
+    });
+
+    test('Não deve cadastrar quando o email já estiver em uso', async ({ request }) => {
+
+        const user = getUser();
+
+        const preCondition = await register.createUser(user);
+        expect(preCondition.status()).toBe(201);
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', 'Este e-mail já está em uso. Por favor, tente outro.');
+    });
+
+    test('Não deve cadastrar quando o email é incorreto', async ({ request }) => {
+
+        const user = {
+            "name": "Jose Luis Amancio",
+            "email": "amancio.jluis&email.com",
+            "password": "123456"
+        };
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', "O campo 'Email' deve ser um email válido");
+    });
+
+    test('Não deve cadastrar quando o campo nome não é informado', async ({ request }) => {
+
+        const user = {
+            "email": "amancio.jluis&email.com",
+            "password": "123456"
+        };
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', "O campo 'Name' é obrigatório");
+    });
+
+    test('Não deve cadastrar quando o campo Email não é informado', async ({ request }) => {
+
+        const user = {
+            "name": "Jose Luis Amancio",
+            "password": "123456"
+        };
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', "O campo 'Email' é obrigatório");
+    });
+
+    test('Não deve cadastrar quando o campo Password não é informado', async ({ request }) => {
+
+        const user = {
+            "name": "Jose Luis Amancio",
+            "email": "amancio.jluis@email.com"
+        };
+
+        const response = await register.createUser(user);
+        expect(response.status()).toBe(400);
+
+        const responseBody = await response.json();
+        expect(responseBody).toHaveProperty('message', "O campo 'Password' é obrigatório");
+    });
+
+});
